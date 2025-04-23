c78ccc5871154a883aa7b58a64063476d9d73236
Qasim Ali <qasimaliwork65@gmail.com>
2025-04-23 19:26:10 +0500

    chore: update vite.config.js for improved build configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000'
-    }
-  }
+      '/api': 'http://localhost:5000',
+    },
+  },
+  build: {
+    outDir: 'dist',
+  },
 })
