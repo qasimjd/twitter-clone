0f014239b53b143032e1c53ffd4b885bf9834a8a
Qasim Ali <qasimaliwork65@gmail.com>
2025-04-23 19:12:26 +0500

    refactor: clean up index.js and update vercel.json for improved routing

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-import path from 'path';
-
 
 import authRoutes from './routes/auth.routes.js';
 import userRoutes from './routes/user.routes.js';
@@ -13,26 +11,15 @@ import connectDB from './lib/db.js';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
-const __dirname = path.resolve();
 
 app.use(express.json({ limit: '5mb' }));
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/user", userRoutes);
-app.use("/api/posts", postRoutes);
-app.use("/api/notifications", notificationRoutes);
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/dist')));
-
-    app.get('*', (_, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
-    });
-}
-
 connectDB();
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+
+app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/posts', postRoutes);
+app.use('/api/notifications', notificationRoutes);
+
+export default app;
